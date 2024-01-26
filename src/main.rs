b9478aad9361deee5160ff2820c17637cd252add
jerome <jerome.bousquie@rodez.fr>
2024-01-26 10:18:15 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use actix_web::{get, web, App, HttpServer, Result};
+use actix_files::Files;
+
+/// extract path info from "/users/{user_id}/{friend}" url
+/// {user_id} - deserializes to a u32
+/// {friend} - deserializes to a String
+#[get("/users/{user_id}/{friend}")] // <- define path parameters
+async fn index(path: web::Path<(u32, String)>) -> Result<String> {
+    let (user_id, friend) = path.into_inner();
+    Ok(format!("Welcome {}, user_id {}!", friend, user_id))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(Files::new("/static", "./static/").show_files_listing()))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
